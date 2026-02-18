df2146c26d20a604e712ee54c1f733fe80d45698
labstraction <labstraction@protonmail.com>
2026-02-18 10:02:23 +0100

    xml request

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 
+const http = new XMLHttpRequest();
+
+http.onreadystatechange = callback
+
+xhttp.open("GET", url, true);
+
+xhttp.send();
+
+function callback(){
+    if (this.readyState == 4 && this.status == 200) {
+       console.log(http.responseText);
+    }
+}
